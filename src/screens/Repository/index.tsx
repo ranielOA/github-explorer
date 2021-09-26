3ef7c26918cc3973797f49dfd5a0fe4848391f85
ranielOA <r.a_tw@hotmail.com>
2021-09-26 19:17:46 -0300

    repository

--- src/screens/Repository/index.tsx
+++ src/screens/Repository/index.tsx
@@ -36,55 +36,69 @@ export function Repository() {
   const { findRepositoryById } = useRepositories();
   const repository = findRepositoryById(repositoryId);
 
-  function handleIssueNavigation(issueUrl: string) {
+  const handleIssueNavigation = (issueUrl: string) => () => {
     // TODO - use Linking to open issueUrl in a browser
-  }
+    Linking.openURL(issueUrl);
+  };
 
   return (
     <Background>
       <Container>
         <RepoInfo>
-          {/* <OwnerAvatar source={{ uri:  }} /> */}
+          <OwnerAvatar source={{ uri: repository.owner.avatar_url }} />
 
           <TextGroup>
             <TitleAnimation>
               {
                 // TODO - full name of the repository
+                repository.full_name
               }
             </TitleAnimation>
 
-            <Description numberOfLines={2}>{
-              //TODO - repository description
-            }</Description>
+            <Description numberOfLines={2}>
+              {
+                //TODO - repository description
+                repository.description
+              }
+            </Description>
           </TextGroup>
         </RepoInfo>
 
         <RepoStats>
           <Stars>
-            <StarsCounter>{
-              // TODO - repository stargazers count
-            }</StarsCounter>
+            <StarsCounter>
+              {
+                // TODO - repository stargazers count
+                repository.stargazers_count
+              }
+            </StarsCounter>
             <StarsText>Stars</StarsText>
           </Stars>
 
           <Forks>
-            <ForksCounter>{
-              // TODO - repository forks count
-            }</ForksCounter>
+            <ForksCounter>
+              {
+                // TODO - repository forks count
+                repository.forks_count
+              }
+            </ForksCounter>
             <ForksText>Forks</ForksText>
           </Forks>
 
           <OpenIssues>
-            <OpenIssuesCounter>{
-              // TODO - repository issues count
-            }</OpenIssuesCounter>
+            <OpenIssuesCounter>
+              {
+                // TODO - repository issues count
+                repository.issues.length
+              }
+            </OpenIssuesCounter>
             <OpenIssuesText>Issues{'\n'}Abertas</OpenIssuesText>
           </OpenIssues>
         </RepoStats>
 
         <IssuesList
           data={repository.issues}
-          keyExtractor={issue => String(issue.id)}
+          keyExtractor={(issue) => String(issue.id)}
           showsVerticalScrollIndicator={false}
           renderItem={({ item: issue }) => (
             <Card
@@ -93,11 +107,12 @@ export function Repository() {
                 title: issue.title,
                 subTitle: issue.user.login,
               }}
-            // TODO - onPress prop calling 
+              // TODO - onPress prop calling
+              onPress={handleIssueNavigation(issue.html_url)}
             />
           )}
         />
       </Container>
     </Background>
-  )
+  );
 }
